54771d2463966bced30d22c98e0ebcf630aa50ba
Connor FitzGerald <connorreecefitzgerald@gmail.com>
2019-02-05 15:49:25 -0500

    cleaned up the food api code(had duplicate fetch lol)

--- problems/foods-json/src/script.js
+++ problems/foods-json/src/script.js
@@ -1,28 +1,20 @@
-buildString = ""
 fetch("http://localhost:8088/foods")
-    .then(foods => foods.json())
-    .then(parsedFoods => {
-       
-        parsedFoods.forEach(foods => {
-          console.log(foods)
-        })
-        // document.querySelector("#foodList").innerHTML=buildString
-    })
-
-    fetch("http://localhost:8088/foods")
     .then(response => response.json())
     .then(myParsedFoods => {
         myParsedFoods.forEach(foods => {
-            console.log(foods) 
+            console.log(foods)
             fetch(`https://world.openfoodfacts.org/api/v0/product/${foods.barcode}.json`)
                 .then(response => response.json())
                 .then(productInfo => {
                     foods.ingredients = productInfo.product.ingredients_text_en
                     console.log(foods.ingredients)
-                    buildString += `<section> <div id="nameID">${foods.name}</div><div id= "catFood"> ${foods.category}</div> <div id ="ethFood">${foods.ethnicity}</div><div id ="idFood">${foods.ingredients}</div></section>`
-                        document.querySelector("#foodList").innerHTML=buildString
-                    })
-                        
-                    
+                    document.querySelector("#foodList").innerHTML +=
+                    `<section> 
+                        <div id="nameID">${foods.name}</div>
+                        <div id= "catFood"> ${foods.category}</div> 
+                        <div id ="ethFood">${foods.ethnicity}</div>
+                        <div id ="idFood">${foods.ingredients}</div>
+                    </section>`
                 })
         })
+    })
--- problems/wondertwins/powers.js
+++ problems/wondertwins/powers.js
@@ -1,22 +1,30 @@
-document.querySelector("#activate-flight").addEventListener("click", activatePower)
+document.querySelector("#activate-flight").addEventListener("click", flightHandlerFunction);
 
-document.querySelector("#activate-mindreading").addEventListener("click", activatePower)
+document.querySelector("#activate-mindreading").addEventListener("click", mindReadingFunction);
 
-// function flightHandlerFunction(){
-//    const flightSection = document.querySelector("#flight")
-//    flightSection.classList.replace("disable", "enable");
-// }
+document.querySelector("#activate-xray").addEventListener("click", xrayFunction);
 
-// function mindReadingFunction(){
-//     const mindReadingSection = document.querySelector("#mindreading")
-//     mindReadingSection.classList.replace("disable", "enable");
-// }
+function flightHandlerFunction(){
+   const flightSection = document.querySelector("#flight")
+   flightSection.classList.replace("disabled", "enabled");
+}
 
-// flightHandlerFunction()
-// mindReadingFunction()
+function mindReadingFunction(){
+    const mindReadingSection = document.querySelector("#mindreading")
+    mindReadingSection.classList.replace("disabled", "enabled");
+}
 
-function activatePower(event){
-    const idOfSection = event.target.id.split("-")[1]   
-    const sectionToChange = document.querySelector
-    // sectionToChange.classList.replace("disabled", "enabled")
+function xrayFunction(){
+    const xraySection = document.querySelector("#xray")
+    xraySection.classList.replace("disabled", "enabled");
+}
+
+document.querySelector("#activate-all").addEventListener("click", activateAllFunction)
+document.querySelector("#deactivate-all").addEventListener("click", deactivateAllFunction)
+
+function activateAllFunction(){
+    const allOfEm = document.querySelectorAll(".power");
+    allOfEm.forEach()
+
+    }
 }
